447a3acbefa993179ab811e3744b1136918b3f12
Robin Scherrer <robin.scherrer@abraxas.ch>
2026-05-19 10:18:36 +0200

    chore: update domain

--- cmd/main.go
+++ cmd/main.go
@@ -186,7 +186,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "dba2c36a.example.ch",
+		LeaderElectionID:       "agent-forge-operator.containeroo.ch",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -42,7 +42,7 @@ var (
 
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
-	projectImage = "example.com/agent-forge-operator:v0.0.11"
+	projectImage = "ghcr.io/containeroo/agent-forge-operator:e2e"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
